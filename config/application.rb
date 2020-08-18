d479e7aaa751869c1044bc359322184480366d6e
Pony <Pony1125@outlook.com>
2020-08-18 11:54:47 -0400

    Update cors

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ Bundler.require(*Rails.groups)
 module CmsApi
   class Application < Rails::Application
     
-    config.middleware.insert_before 0, "Rack::Cors" do
+    config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
         resource '*', :headers => :any, :methods => [:get, :post, :options]
